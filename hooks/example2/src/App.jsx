bfc5fcc95103741c781a40d73cbf166f4e79ed87
parkyoungseo <givpro22@daum.net>
2024-12-13 00:36:12 +0900

    24/12/13

---
+++ hooks/example2/src/App.jsx
@@ -0,0 +1,25 @@
+
+function App() {
+  
+
+  return (
+    <main>
+      <div id ="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">player1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">player2</span>
+            <span className="player-symbol">0</span>
+          </li>
+
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  )
+}
+
+export default App
