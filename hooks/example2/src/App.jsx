6306e84b1437066cc9de7ed2c8373f9f2e6df718
parkyoungseo <givpro22@daum.net>
2024-12-16 01:26:42 +0900

    24/12/16

--- hooks/example2/src/App.jsx
+++ hooks/example2/src/App.jsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player.jsx";
+import Log from "./components/Log.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X'
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X"){
+    currentPlayer = 'O'
+  }
+
+  return currentPlayer
+}
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X')
+  const [gameTurns, setGameTurns] = useState([])
+  // const [activePlayer, setActivePlayer] = useState('X')
+  const activePlayer = deriveActivePlayer(gameTurns)
 
-  function handleSelectSquare() {
-    setActivePlayer((curActiePlayer) => curActiePlayer === 'X' ? 'O' : 'X')
+  function handleSelectSquare(rowIndex, colIndex) {
+
+  const  currentPlayer = deriveActivePlayer(gameTurns)
+  ///setActivePlayer((curActiePlayer) => curActiePlayer === 'X' ? 'O' : 'X')
+    setGameTurns(prevTurns => {
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
+        ...prevTurns]
+
+      return updatedTurns
+    })
   }
 
   return (
@@ -16,8 +39,12 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer ==='X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer ==='O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard 
+        onSelectSquare={handleSelectSquare} 
+        turns = {gameTurns}
+        />
       </div>
+      <Log turns={gameTurns}/>
     </main>
   )
 }
